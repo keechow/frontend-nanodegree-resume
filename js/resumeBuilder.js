fab03ec3a3d6bde2105fbae0d62aad4e74638e55
keechow <keechow@gmail.com>
2016-02-02 16:47:00 +0800

    2.2.16 Lesson 1: append, prepend in js
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	modified:   index.html
    	modified:   js/resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,12 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
- */
+ 
+ $("#main").append(["ECHO TELION PROJECT"]);
+*/
+
+var formattedName = HTMLheaderName.replace("%data%", "Low Kee Chow");
+var formattedRole = HTMLheaderRole.replace("%data%","Web Developer");
+
+$("#main").append(formattedName);
+
+$("#main").append(formattedRole);
