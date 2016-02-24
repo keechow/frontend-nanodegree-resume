4ff8527aea89b1b53bded04d094dc2d607be12ab
keechow <keechow@gmail.com>
2016-02-24 14:49:16 +0800

    On branch master  Your branch is up-to-date with 'origin/master'.  Changes to be committed: 	modified:   js/resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,11 +2,16 @@
 This is empty on purpose! Your code to build the resume will go here.
  
  $("#main").append(["ECHO TELION PROJECT"]);
-*/
+
 
 var formattedName = HTMLheaderName.replace("%data%", "Low Kee Chow");
 var formattedRole = HTMLheaderRole.replace("%data%","Web Developer");
 
-$("#main").append(formattedName);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
 
-$("#main").append(formattedRole);
+
+*/
+var str_audacity = "Audacity"
+var str_udacity = str_audacity.slice(2);
+
