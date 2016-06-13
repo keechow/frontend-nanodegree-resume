32af0cf10a7ad23901ae44002ed4a0d4d0a2a128
keechow <keechow@gmail.com>
2016-06-14 06:40:53 +0800

    reorganize files

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -12,6 +12,60 @@ $("#header").prepend(formattedName);
 
 
 */
+
+/*
 var str_audacity = "Audacity"
 var str_udacity = str_audacity.slice(2);
+var myName = "Echo Telion Project";
+var myRole = "Ask. Seek. Knock";
+
+
+
+var formattedName = HTMLheaderName.replace("%data%", myName);
+var formattedRole = HTMLheaderRole.replace("%data%", myRole);
+
+var mySkillList = ["fast-learner, ","fast-worker, ","natural-born-leader ","smart-thinker ",""];
+*/
+var bio = {
+	"name" : "Echo Telion",
+	"motto" : "Ask. Seek. Knock.",
+	"skill" : ["stubborn", "gold evaluation", "thinking deep", "linking things up"],
+	"contacts" : {
+		"mobile" : "0126549088",
+		"email" : "echo.telion@gmail.com",
+		"github" : "keechow",
+		"location" : "Kajang"
+	},
+	"welcomeMessage" : "Echo Telion Project."
+
+};
+
+var education = {
+
+	"school" : [
+		{
+			"name" : "Kajang High School",
+			"city" : "Kajang",
+			"degree" : "SPM",
+			"majors" : "Survival Skills"
+		},
+		{
+			"name" : "Uniten",
+			"city" : "Kajang",
+			"degree" : "PTPTN Loan agreement",
+			"majors" : "How to get into debt early in your life"
+
+		}
+	],
+	"online courses" : [
+		{
+			"name" : "Javascript Basics",
+			"provider" : "Udacity",
+			"category" : "web development"
+		}
+	]
+};
+
+
+
 
